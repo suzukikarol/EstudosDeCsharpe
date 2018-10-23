228e67ab7b729d0be1b554830fc1b136a0ed92ac
Karoline Suzuki <42789906+suzukikarol@users.noreply.github.com>
2018-10-23 19:45:08 -0300

    Add files via upload
    C# inicial , em forma estruturado ainda.

---
+++ 10-CalculaPoupanca/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 - calcula poupança");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            //while parece com if, porem ele executa como loop enqnt a condição for satisfeita.
+            while (contadorMes <= 12){
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após "+contadorMes+" mês você terá R$ " + valorInvestido);
+
+                // contadorMes = contadorMes + 1;//soma de um em um
+                // contadorMes += 1; //pega a varialvel da esq e conta mais um
+                contadorMes++;
+            }
+
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 8 - Condicionais2");
+
+            int idadeJoao = 16;
+            int quantidadePessoas = 2;
+            bool acompanhado = quantidadePessoas >= 2;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "João está acompanhado";
+            }
+            else
+            {
+                mensagemAdicional = "João não está acompanhado";
+            }
+
+
+            if (idadeJoao >= 18 || acompanhado == true) 
+            { 
+                Console.WriteLine("Pode entrar!");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar!");
+                Console.WriteLine(mensagemAdicional);
+            }
+
+          
+
+            Console.ReadLine();
+        }
+    }
+}
